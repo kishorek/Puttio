924a6115403f6edc031393a10af19c6d222baeb1
KishoreK <ukishore196@yahoo.co.in>
2013-07-17 14:26:41 +0800

    Added error message to add API Keys

---
+++ APP_SECRET.h
@@ -0,0 +1,14 @@
+//
+//  APP_SECRET_EX.h
+//  Puttio
+//
+//  Created by orta therox on 25/03/2012.
+//  Copyright (c) 2012 ortatherox.com. All rights reserved.
+//
+
+// https://api.put.io/v2/docs/
+// Sign up for these creds here https://put.io/v2/oauth2/register
+
+#error Add your API Keys for Put.io
+static NSString* APP_SECRET = @"";
+static NSString* APP_ID = @"";
